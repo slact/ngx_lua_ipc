f963207d9af3f5a95060cf216d1a7a60315caf66
Leo P <junk@slact.net>
2017-02-25 21:56:06 -0500

    no utils needed right now

--- src/ngx_lua_ipc_util.c
+++ src/ngx_lua_ipc_util.c
@@ -1,12 +1,3 @@
 #include <ngx_lua_ipc.h>
 #include <assert.h>
 
-ngx_int_t nchan_init_timer(ngx_event_t *ev, void (*cb)(ngx_event_t *), void *pd) {
-#if nginx_version >= 1008000
-  ev->cancelable = 1;
-#endif
-  ev->handler = cb;
-  ev->data = pd;
-  ev->log = ngx_cycle->log;
-  return NGX_OK;
-}
